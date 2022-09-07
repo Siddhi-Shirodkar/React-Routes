35238991973e32c75572dd9ab4d9fe9241194c60
Siddhi <siddhishirodkar2018@gmail.com>
2022-09-07 16:11:56 +0530

    Second Commit

---
+++ src/pages/blogs.js
@@ -0,0 +1,5 @@
+const Blogs = () => {
+    return <h1>This is Blog Articles</h1>;
+  };
+  
+  export default Blogs;
---
+++ src/pages/home.js
@@ -0,0 +1,5 @@
+const Home = () => {
+    return <h1>  This is the Home pages </h1>;
+  };
+  
+  export default Home;
